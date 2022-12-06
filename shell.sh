d3bb4cc9dfe3fbf144ecf87e1486a5e1bfc08731
Kimbley <kimbley@shardeum.org>
2022-12-06 09:15:29 -0600

    the idea of three seperate images for operators

--- shell.sh
+++ shell.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
 
-docker exec -it shardeum-dashboard /bin/bash
+if [ $# -eq 0 ]; then
+    echo "Entering operator-cli container.";
+    docker exec -it shardeum-dashboard_cli_1 /bin/bash;
+else
+    while (( "$#" )); do
+        case "$1" in
+            c) echo "Entering operator-cli container."
+                docker exec -it shardeum-dashboard_cli_1 /bin/bash
+                ;;
+            g) echo "Entering operator-gui container."
+                docker exec -it shardeum-dashboard_gui_1 /bin/bash
+                ;;
+            v) echo "Entering validator container."
+                docker exec -it shardeum-dashboard_validator_1 /bin/bash
+                ;;
+            *) echo -e "Use ./shell.sh with the following flags."
+                echo -e " no flag to enter the cli container."
+                echo -e " 'c' flag to enter the cli container."
+                echo -e " 'g' flag to enter the gui container."
+                echo -e " 'v' flag to enter the validator container."
+                ;;
+        esac
+        shift
+    done
+
+fi
